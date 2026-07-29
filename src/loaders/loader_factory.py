182322fe034b2c662a0bc153ae37b10f864f1f22
touchstone7 <rk2real@gmail.com>
2026-07-29 10:06:13 +0530

    cleaned up loader factory

--- src/loaders/loader_factory.py
+++ src/loaders/loader_factory.py
@@ -1,17 +1,10 @@
 from models import Document
 
 from loaders.txt_loader import load_txt_documents
-from loaders.markdown_loader import (
-    load_markdown_documents
-)
+from loaders.markdown_loader import load_markdown_documents
+from loaders.pdf_loader import load_pdf_documents
+from loaders.docx_loader import load_docx_documents
 
-from loaders.pdf_loader import (
-    load_pdf_documents
-)
-
-from loaders.docx_loader import (
-    load_docx_documents
-)
 
 def load_documents(
     directory: str
@@ -19,19 +12,16 @@ def load_documents(
 
     documents: list[Document] = []
 
-    documents.extend(
-        load_txt_documents(directory)
-    )
+    loaders = [
+        load_txt_documents,
+        load_markdown_documents,
+        load_pdf_documents,
+        load_docx_documents,
+    ]
 
-    documents.extend(
-        load_markdown_documents(directory)
-    )
-
-    documents.extend(
-        load_pdf_documents(directory)
-    )
-    documents.extend(
-        load_docx_documents(directory)
-    )
+    for loader in loaders:
+        documents.extend(
+            loader(directory)
+        )
 
     return documents
